17105ec0ac9ca7566b4ba0f76e07af3d741a6ff3
gba-3 <gbayru3@gmail.com>
2021-05-24 23:51:11 +0900

    DB接続処理作成

---
+++ infrastructure/sqlx.go
@@ -0,0 +1,15 @@
+package infrastructure
+
+import "github.com/jmoiron/sqlx"
+
+type MySQL struct {
+	DB *sqlx.DB
+}
+
+func NewMySQL(DB *sqlx.DB) *MySQL {
+	return &MySQL{DB}
+}
+
+func (m *MySQL) Select(dest interface{}, query string, args ...interface{}) error {
+	return m.DB.Select(dest, query, args)
+}
