f35b069df9a1e63b87c46f26139cfc16a692d5bc
gba-3 <gbayru3@gmail.com>
2021-05-27 01:10:05 +0900

    データ追加用メソッド作成

--- infrastructure/sqlx.go
+++ infrastructure/sqlx.go
@@ -13,3 +13,9 @@ func NewMySQL(DB *sqlx.DB) *MySQL {
 func (m *MySQL) Select(dest interface{}, query string, args ...interface{}) error {
 	return m.DB.Select(dest, query, args...)
 }
+
+func (m *MySQL) Exec(query string, args ...interface{}) error {
+	tx := m.DB.MustBegin()
+	tx.MustExec(query, args...)
+	return tx.Commit()
+}
