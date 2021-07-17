04f5e732c3eb70326451dba53d53845bb7dacecf
gba-3 <gbayru3@gmail.com>
2021-07-17 15:53:52 +0900

    トークン有効期限をJSTに修正

--- auth/jwt.go
+++ auth/jwt.go
@@ -1,17 +1,22 @@
 package auth
 
 import (
-	jwtmiddleware "github.com/auth0/go-jwt-middleware"
-	"github.com/form3tech-oss/jwt-go"
 	"os"
 	"time"
+
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/form3tech-oss/jwt-go"
 )
 
 func CreateToken(email string) (string, error) {
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		return "", err
+	}
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
 		"email": email,
 		"iat":   time.Now(),
-		"exp":   time.Now().Add(time.Hour * 24).Unix(),
+		"exp":   time.Now().In(jst).Add(time.Minute * 3).Unix(),
 	})
 	// 電子署名
 	tokenString, err := token.SignedString([]byte(os.Getenv("SIGNINKEY")))
